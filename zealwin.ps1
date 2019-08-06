4a14f235d86d72026fd89e00cab6613f9a8d33c8
Deepak Baghel <31988315+zealcox@users.noreply.github.com>
2019-08-06 21:56:11 +0530

    version1

---
+++ zealwin.ps1
@@ -0,0 +1,3 @@
+﻿$x = (iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')))
+$y = (iex (new-object net.webclient).downloadstring('https://get.scoop.sh'))
+$z = (scoop install aria2)
